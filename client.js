9cf8c64f30081475ff5d24fe6952625b3239690e
John Gaffaney <87045628+jgaffaney@users.noreply.github.com>
2021-09-28 13:53:26 -0500

    copmpleted ratingBonus function

--- client.js
+++ client.js
@@ -39,4 +39,34 @@ const employees = [
 // This is not a race. Everyone on your team should understand what is happening.
 // Ask questions when you don't.
 
+//a master function to run all subfunctions and create a result object from the results 
+
+
+
+//a function that takes in a rating and returns a bonus percentage
+function ratingBonus(rating) {
+  if(rating <= 2) {
+    return 0;
+  } else if (rating <= 3) {
+    return 0.04
+  } else if(rating <= 4) {
+    return  0.06;
+  } else if (rating === 5) {
+    return 0.1;
+  }
+}//end ratingBonus
+console.log(ratingBonus(5));
+
+
+
+//a function that takes in an employee number and returns an additional bonus if employee number has 4 digits
+
+
+
+//a function that takes in an annual income and reduces bonus percentage to 1% if annual income > $65,000
+
+
+
+//a function that checks bonus percent and reduces to 13% if > 13%
+
 console.log( employees );
